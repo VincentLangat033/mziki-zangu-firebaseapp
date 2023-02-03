b95cfcec849763572e1345b8535a401d143b93fe
Vincent Langat <kimutaiketer033@gmail.com>
2023-02-03 09:40:03 +0300

    add firebase config files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,42 @@
-// const { Console } = require("console");
+import { initializeApp } from 'firebase/app'
+import {
+    getFirestore, collection, getDocs
+} from 'firebase/firestore'
 
-// Console.log("Hello from Mziki App ")
-console.log("Hello from Mziki App ")
+const firebaseConfig = {
+    apiKey: "AIzaSyAocO5ICpbpe91b9h8MRtXlFE1rHG4Kscc",
+    authDomain: "mziki-zangu-firebase-app.firebaseapp.com",
+    projectId: "mziki-zangu-firebase-app",
+    storageBucket: "mziki-zangu-firebase-app.appspot.com",
+    messagingSenderId: "927210747647",
+    appId: "1:927210747647:web:d5fcf88cc3c96c27f87eca",
+    measurementId: "G-Q9S3XX3WHV"
+  };
+
+//    Initialize firebase app
+initializeApp(firebaseConfig)
+
+
+//  Initialize Firestore services
+const db = getFirestore()
+
+
+//  get a reference to a specific collection in our database
+const colRef = collection( db, 'songs')
+
+
+// get collection data
+getDocs(colRef)
+ .then((snapshot)=>{
+    console.log(snapshot.docs)
+    let songs = []
+    snapshot.docs.forEach((doc)=>{
+        songs.push({
+            ...doc.data(), id: doc.id
+        })
+    })
+console.log(songs)
+ })
+ .catch(err =>{
+    console.log(err.message)
+ })
